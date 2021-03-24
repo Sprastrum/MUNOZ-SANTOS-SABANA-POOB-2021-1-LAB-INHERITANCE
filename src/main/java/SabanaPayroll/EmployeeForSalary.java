c9d1dfbd08e607f7bfedf4de179dd20240d63623
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-03-24 18:06:57 -0500

    Update EmployeeForSalary.java

--- src/main/java/SabanaPayroll/EmployeeForSalary.java
+++ src/main/java/SabanaPayroll/EmployeeForSalary.java
@@ -2,11 +2,24 @@ package SabanaPayroll;
 
 public class EmployeeForSalary extends Employee {
 
-    private final double HEALTH_BENEFIT = 0.04;
-    private final double PENSION_BENEFIT= 0.04;
+    private static final double HEALTH_BENEFIT = 0.04;
+    private static final double PENSION_BENEFIT= 0.04;
+    private double salary;
 
     public EmployeeForSalary(String name, String lastname, Department department, Double salary) {
         super(name,lastname,department);
-    
+        this.salary=calculateSalary(salary);
+
+    }
+
+    public double calculateSalary(Double salary) {
+        return salary-(salary*HEALTH_BENEFIT)-(salary*PENSION_BENEFIT);
+    }
+
+    public double getHealthDiscount(){
+        return this.salary*HEALTH_BENEFIT;
+    }
+    public double getPensionDiscount(){
+        return this.salary*PENSION_BENEFIT;
     }
 }
